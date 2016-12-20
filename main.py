2220b06105c80a7c46bcf6d2038b85dbd5b043ad
CQY <pkudysj@126.com>
2016-12-20 20:34:26 +0800

    col_idx_of_Y

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ data_fn = 'data/Soybean/soybean-large.data.txt'
 # read data with dtype = str
 X = pd.read_csv(data_fn, sep=',', header=None, dtype=str, skiprows=None, na_values='?', keep_default_na=False)
 X.dropna(inplace=True)
-Y = X[0]
+col_idx_of_Y = 0
+Y = X[col_idx_of_Y]
 K = len(set(Y))
-X.drop(0, axis=1, inplace=True)
+X.drop(col_idx_of_Y, axis=1, inplace=True)
 
 emnb = EMNaiveBayes(epsilon=1e-5)
 emnb.fit(X.values, K)
