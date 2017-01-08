4d0f63345209a675a26785cbf47830d88a55458f
CQY <pkudysj@126.com>
2017-01-08 15:22:13 +0800

    remove some "import"

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-import sys, os, collections, copy
-import numpy as np
 import pandas as pd
-from pandas import DataFrame, Series
 
 from EMNaiveBayes import *
 
 data_fn = 'data/Soybean/soybean-large.data.txt'
+col_idx_of_Y = 0
 # read data with dtype = str
 X = pd.read_csv(data_fn, sep=',', header=None, dtype=str, skiprows=None, na_values='?', keep_default_na=False)
 X.dropna(inplace=True)
-col_idx_of_Y = 0
 Y = X[col_idx_of_Y]
 K = len(set(Y))
 X.drop(col_idx_of_Y, axis=1, inplace=True)
