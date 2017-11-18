772651d94cb2a6d08d69a92adf7061848eb4cbb9
CQY <qychen@pku.edu.cn>
2017-11-18 23:49:07 +0800

    support py2

--- EMNaiveBayes.py
+++ EMNaiveBayes.py
@@ -1,4 +1,8 @@
 """Implementation of Unsupervised Naive Bayes with EM Algorithm."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import copy
 
 import numpy as np
